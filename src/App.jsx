d30b904917299a62024482d08142ff500f23a748
razikovshukhrat-o <razikovshukhrat@gmail.com>
2026-01-28 14:53:54 +0500

    Yaratib qoydim

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Seaction from './components/Seaction'
 
 function App() {
 
   return (
     <>
-
+    <Header/>
+    <Seaction/>
+    <Footer/>
     </>
   )
 }
